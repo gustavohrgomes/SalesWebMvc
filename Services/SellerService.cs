6624799478c7bc4d5cbbe44b78af32ddf888cd6a
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-08-11 22:03:37 -0300

    SellerFormViewModel and Department select component

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -10,10 +10,7 @@ namespace SalesWebMvc.Services
     {
         private readonly SalesWebMvcContext _context;
 
-        public SellerService(SalesWebMvcContext context)
-        {
-            _context = context;
-        }
+        public SellerService(SalesWebMvcContext context) => _context = context;
 
         public List<Seller> FindAll()
         {
@@ -22,7 +19,6 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller obj)
         {
-            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
