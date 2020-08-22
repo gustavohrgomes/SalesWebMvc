9df682b423d81614938a12ad71b75228d7221609
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-08-22 01:42:18 -0300

    Create Update & Edit method

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -81,5 +81,23 @@ namespace SalesWebMvc.Controllers
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Edit(int? id) 
+        {
+            if (id == null) 
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
     }
 }
